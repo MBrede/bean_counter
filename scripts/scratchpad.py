642afd1be8e8b917eabfc908995120fa21e3eadd
MBrede <bots@max-bre.de>
2022-06-06 17:36:52 +0200

    slic

--- scripts/scratchpad.py
+++ scripts/scratchpad.py
@@ -39,3 +39,52 @@ img = cv2.fastNlMeansDenoising(
         searchWindowSize=21,
     )
 cv2.imwrite("../work/imgs/nlMean_30_7_21.png",img)
+
+import _nCuts as nc
+import numpy as np
+import pandas as pd
+path = "../work/imgs/geometric.png"
+img = cv2.imread(path, 0)
+df = pd.DataFrame(
+    {
+        "value": img.flatten(),
+        "x": np.tile(range(1, img.shape[1] + 1, 1), img.shape[0]),
+        "y": np.repeat(range(1, img.shape[0] + 1, 1), img.shape[1])
+    }
+)
+def fit_export_slic(df, compactness, n_segments):
+    model = nc.SLIC(compactness, n_segments)
+    img = model.fit_predict(df, flatten=False)
+    img = img/n_segments * 256
+    cv2.imwrite(f"../work/imgs/slic_{int(compactness*100)}_{n_segments}.png",img)
+    
+fit_export_slic(df, 0.05, 250)
+fit_export_slic(df, 0.05, 500)
+fit_export_slic(df, 2, 250)
+fit_export_slic(df, 2, 500)
+
+
+from skimage.future import graph
+
+df['label'] = df['x'] + df['y']-1
+out = (
+    df.iloc[:, 1:4]
+    .pivot(index="y", columns="x", values="label")
+    .to_numpy()
+)
+g = graph.rag_mean_color(img, out, mode='similarity', sigma=0.1)
+import networkx as nx
+from matplotlib import pyplot as plt
+def display(g, title):
+    """Displays a graph with the given title."""
+    pos = nx.circular_layout(g)
+    plt.figure()
+    plt.title(title)
+    nx.draw(g, pos)
+    nx.draw_networkx_labels(g, pos)
+    nx.draw_networkx_edge_labels(g, pos, font_size=20)
+
+display(g, 'test')
+
+out = graph.cut_normalized(out, g,thresh=.01, num_cuts=10)
+cv2.imwrite("../work/imgs/ncuts.png",out)
